e17d7fd46433cf1e623bc79fc81897ca511e3a0e
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2021-01-14 09:14:06 -0500

    Section 4.1

--- csharp/src/Program.cs
+++ csharp/src/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System;
-
+using System.IO;
+using System.Collections.Generic;
 namespace lox
 {
     class Program
     {
-        static void Main(string[] args)
+        //TODO: I don't like how the author is using global static state.
+        private static bool HadError = false;
+
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: lox [script]");
+                return 64;
+            } 
+            else if(args.Length == 1) 
+            {
+                return RunFile(args[0]);
+            } 
+            else
+            {
+                RunPrompt();
+                return 0;
+            }
+        }
+
+        private static int RunFile(string filePath)
+        {
+            var contents = File.ReadAllText(filePath);
+            Run(contents);
+            return HadError ? 65 : 0;
+        }
+
+        private static void RunPrompt()
+        {
+            for(;;)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (!(line is object)) break;
+                Run(line);
+                //reset this so we don't crash the interactive session
+                HadError = false;
+            }
+        }
+
+        private static void Run(string contents)
+        {
+            var scanner = new Scanner(contents);
+            var tokens = scanner.ScanTokens();
+
+            foreach(var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+
+        private static void Error(int line, String message)
+        {
+            Report(line, "", message);
+        }
+
+        private static void Report(int line, string where, string message)
+        {
+            Console.Error.WriteLine($"[line {line}] Error{where}: {message}");
+            HadError = true;
         }
     }
+
+    public class Scanner
+    {
+        private readonly string source;
+
+        public Scanner(string source)
+        {
+            this.source = source;
+        }
+
+        public List<Token> ScanTokens() => new List<Token>();
+    }
+
+    public struct Token{}
 }
