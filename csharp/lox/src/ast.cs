557f6bff472134a0e05b44496f8aaa10c2b15d84
Christopher J. McClellan <chris.mcclellan203@gmail.com>
2021-01-27 10:28:38 -0500

    Add statements to AST

--- csharp/lox/src/ast.cs
+++ csharp/lox/src/ast.cs
@@ -85,4 +85,76 @@ namespace lox.ast {
         public override T Accept<T>(ExprVisitor<T> visitor) =>
             visitor.VisitVariableExpr(this);
     }
+
+    public interface StmtVisitor<T>
+    {
+        T VisitBlockStmt(BlockStmt blockStmt);
+        T VisitClassStmt(ClassStmt classStmt);
+        T VisitExpressionStmt(ExpressionStmt expressionStmt);
+        T VisitFunctionStmt(FunctionStmt functionStmt);
+        T IfStmt(IfStmt ifStmt);
+        T PrintStmt(PrintStmt printStmt);
+        T ReturnStmt(ReturnStmt returnStmt);
+        T VarStmt(VarStmt varStmt);
+        T WhileStmt(WhileStmt whileStmt);
+    }
+
+    public abstract record Stmt
+    {
+        public abstract T Accept<T>(StmtVisitor<T> visitor);
+    }
+
+    public record BlockStmt(List<Stmt> statements) : Stmt
+    {
+        public override T Accept<T>(StmtVisitor<T> visitor) =>
+            visitor.VisitBlockStmt(this);
+    }
+
+    public record ClassStmt(Token name, VariableExpr superClass, List<FunctionStmt> methods) : Stmt
+    {
+        public override T Accept<T>(StmtVisitor<T> visitor) =>
+            visitor.VisitClassStmt(this);
+    }
+
+    public record ExpressionStmt(Expr expr) : Stmt
+    {
+        public override T Accept<T>(StmtVisitor<T> visitor) =>
+            visitor.VisitExpressionStmt(this);
+    }
+
+    public record FunctionStmt(Token name, List<Token> parameters, List<Stmt> body) : Stmt
+    {
+        public override T Accept<T>(StmtVisitor<T> visitor) =>
+            visitor.VisitFunctionStmt(this);
+    }
+
+    public record IfStmt(Expr condition, Stmt thenBranch, Stmt elseBranch) : Stmt
+    {
+        public override T Accept<T>(StmtVisitor<T> visitor) =>
+            visitor.IfStmt(this);
+    }
+
+    public record PrintStmt(Expr expr) : Stmt
+    {
+        public override T Accept<T>(StmtVisitor<T> visitor) =>
+            visitor.PrintStmt(this);
+    }
+
+    public record ReturnStmt(Token keyword, Expr value) : Stmt
+    {
+        public override T Accept<T>(StmtVisitor<T> visitor) =>
+            visitor.ReturnStmt(this);
+    }
+
+    public record VarStmt(Token name, Expr initializer) : Stmt
+    {
+        public override T Accept<T>(StmtVisitor<T> visitor) =>
+            visitor.VarStmt(this);
+    }
+
+    public record WhileStmt(Expr condition, Stmt body) : Stmt
+    {
+        public override T Accept<T>(StmtVisitor<T> visitor) =>
+            visitor.WhileStmt(this);
+    }
 }
